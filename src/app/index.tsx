a6686457df55a9de3313fb3df872ad21d5f18032
hr543 <hr5431898@gmail.com>
2025-08-17 19:07:59 +0530

    third commit setup project structure

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View,Image,Text} from 'react-native'
+import React from 'react'
+import ImagePath from '../constants/ImagePath'
+const Index = () => {
+  return (
+        <View>
+        <Image source={ImagePath.react_logo}/>
+      </View>
+  )
+}
+
+export default Index
